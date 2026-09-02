8e4528592cb763bfdba3f3bb8b678b4223761ef9
Nick Ostrovsky <nick@firedev.com>
2026-09-02 10:14:38 +0700

    threefinger: refresh formula description

--- Formula/threefinger.rb
+++ Formula/threefinger.rb
@@ -1,5 +1,5 @@
 class Threefinger < Formula
-  desc "Three-finger horizontal trackpad swipe to any keyboard shortcut"
+  desc "Three-finger swipe on the Mac trackpad → switch tabs. Or the shortcut of your choice."
   homepage "https://github.com/firedev/threefinger"
   url "https://github.com/firedev/threefinger/archive/refs/tags/v1.1.2.tar.gz"
   sha256 "1bab60168fabaaa414d9d09460757910224bbfb49884dea5cd40418712a1937d"
